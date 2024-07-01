d27fb82e9170b952eacac29c95eb05f052fe29ae
Irwan Fathurrahman <meomancer@gmail.com>
2024-07-01 12:33:54 +0700

    Add missing docstring

--- django_project/gap/tasks/ingestor.py
+++ django_project/gap/tasks/ingestor.py
@@ -1,3 +1,10 @@
+# coding=utf-8
+"""
+Tomorrow Now GAP.
+
+.. note:: Tasks.
+"""
+
 from celery import shared_task
 from celery.utils.log import get_task_logger
 
@@ -8,6 +15,7 @@ logger = get_task_logger(__name__)
 
 @shared_task(bind=True, queue='update')
 def run_ingestor_session(self, _id: int):
+    """Run ingestor."""
     try:
         session = IngestorSession.objects.get(id=_id)
         session.run()
