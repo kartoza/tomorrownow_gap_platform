91049e107da73bbb14af2af5ba92de4c234a8be8
Danang <danangmassandy@gmail.com>
2024-07-02 07:41:37 +0000

    update hyrax entrypoint with s3 variables

--- deployment/hyrax/entrypoint.sh
+++ deployment/hyrax/entrypoint.sh
@@ -7,11 +7,11 @@ if [ -f "$file" ] ; then
 fi
 
 touch "$file"
-echo "tomorrownow = url:http://minio:9000/tomorrownow/" >> "$file"
-echo "tomorrownow += id:minio_tomorrownow" >> "$file"
-echo "tomorrownow += key:minio_tomorrownow" >> "$file"
+echo "tomorrownow = url:${S3_AWS_ENDPOINT}${S3_AWS_BUCKET_NAME}/" >> "$file"
+echo "tomorrownow += id:${S3_AWS_ACCESS_KEY_ID}" >> "$file"
+echo "tomorrownow += key:${S3_AWS_SECRET_ACCESS_KEY}" >> "$file"
 echo "tomorrownow += region:us-east-1" >> "$file"
-echo "tomorrownow += bucket:tomorrownow" >> "$file"
+echo "tomorrownow += bucket:${S3_AWS_ENDPOINT}" >> "$file"
 
 chmod 600 "$file"
 chown bes:bes "$file"
