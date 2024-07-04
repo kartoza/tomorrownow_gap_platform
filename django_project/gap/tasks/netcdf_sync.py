fd71d57c05c9cc34fd7729ec53251f8e8888d6bd
Danang <danangmassandy@gmail.com>
2024-07-05 00:10:23 +0700

    add code to import netcdf to measurements table

--- django_project/gap/tasks/netcdf_sync.py
+++ django_project/gap/tasks/netcdf_sync.py
@@ -8,19 +8,29 @@ Tomorrow Now GAP.
 import os
 from celery.utils.log import get_task_logger
 from datetime import datetime
+from pydap.client import open_url
+from pydap.model import BaseType
 import boto3
 from django.utils import timezone
+from django.contrib.gis.geos import Point
 
 from core.celery import app
 from gap.models import (
+    Country,
     ObservationType,
     Attribute,
+    Measurement,
     Provider,
+    Station,
     NetCDFFile,
     NetCDFProviderMetadata,
     NetCDFProviderAttribute
 )
-from gap.utils.netcdf import NetCDFProvider, CBAM_VARIABLES, SALIENT_VARIABLES
+from gap.utils.netcdf import (
+    NetCDFProvider,
+    CBAM_VARIABLES,
+    SALIENT_VARIABLES,
+)
 
 
 logger = get_task_logger(__name__)
@@ -175,3 +185,71 @@ def netcdf_s3_sync():
     initialize_provider_variables(salient, SALIENT_VARIABLES)
     sync_by_provider(cbam)
     sync_by_provider(salient)
+
+
+@app.task(name="netcdf_readall")
+def netcdf_readall(netcdffile_id):
+    obs_type, _ = ObservationType.objects.get_or_create(
+        name='Satellite Observations'
+    )
+    country = Country.objects.first()
+    netcdf = NetCDFFile.objects.get(id=netcdffile_id)
+    dataset = open_url(netcdf.opendap_url)
+    lat_array = dataset.lat[:].data
+    lon_array = dataset.lon[:].data
+    measurements = {}
+    for variable in CBAM_VARIABLES.keys():
+        attr_data = dataset[variable]
+        print(f'{variable} - has shape: {attr_data.shape}')
+        if isinstance(attr_data, BaseType):
+            arr_data = attr_data
+        else:
+            arr_data = attr_data[variable]
+        attribute_val = CBAM_VARIABLES[variable]
+        attr, _ = Attribute.objects.get_or_create(
+            name=attribute_val.name,
+            defaults={
+                'description': attribute_val.desc
+            }
+        )
+        measurements[variable] = {
+            'data': arr_data[0, :, :],
+            'attribute': attr,
+            'unit': attribute_val.unit
+        }
+        Measurement.objects.filter(
+            station__provider=netcdf.provider,
+            attribute=attr,
+            date_time=netcdf.start_date_time
+        ).delete()
+    point_idx = 1
+    total_measurements = 0
+    for idx_lon, lon in enumerate(lon_array):
+        for idx_lat, lat in enumerate(lat_array):
+            p = Point(x=lon, y=lat)
+            netcdfpoint_id = f'CBAM-{netcdffile_id}-{point_idx}'
+            station, _ = Station.objects.get_or_create(
+                geometry=p,
+                provider=netcdf.provider,
+                observation_type=obs_type,
+                defaults={
+                    'code': netcdfpoint_id,
+                    'country': country,
+                    'name': netcdfpoint_id
+                }
+            )
+            results = []
+            for variable in CBAM_VARIABLES.keys():
+                results.append(Measurement(
+                    station=station,
+                    attribute=measurements[variable]['attribute'],
+                    unit=measurements[variable]['unit'],
+                    date_time=netcdf.start_date_time,
+                    value=measurements[variable]['data'][0][idx_lat][idx_lon].data
+                ))
+            if results:
+                Measurement.objects.bulk_create(results)
+                total_measurements += len(results)
+                if total_measurements % 1000 == 0:
+                    print(f'Created measurements {total_measurements}')
+            point_idx += 1
