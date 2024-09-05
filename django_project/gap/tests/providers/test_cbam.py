29a33a09877dcee738619e3e1b7c735962943c5f
Danang <danangmassandy@gmail.com>
2024-09-05 09:31:25 +0000

    add test cbam zarr reader

--- django_project/gap/tests/providers/test_cbam.py
+++ django_project/gap/tests/providers/test_cbam.py
@@ -8,16 +8,21 @@ Tomorrow Now GAP.
 from django.test import TestCase
 from datetime import datetime
 import xarray as xr
-from django.contrib.gis.geos import Point
+import numpy as np
+import pandas as pd
+from django.contrib.gis.geos import Point, MultiPoint, Polygon, MultiPolygon
 from unittest.mock import Mock, patch
 
 from core.settings.utils import absolute_path
-from gap.utils.reader import DatasetReaderInput
+from gap.models.dataset import Dataset
+from gap.models.measurement import DatasetAttribute
+from gap.utils.reader import DatasetReaderInput, LocationInputType
 from gap.utils.netcdf import (
     NetCDFProvider,
 )
 from gap.providers import (
-    CBAMNetCDFReader
+    CBAMNetCDFReader,
+    CBAMZarrReader
 )
 from gap.factories import (
     ProviderFactory,
@@ -89,3 +94,110 @@ class TestCBAMNetCDFReader(TestCase):
             self.assertAlmostEqual(
                 data_value['data'][0]['values']['max_temperature'],
                 33.371735, 6)
+
+
+class TestCBAMZarrReader(TestCase):
+    """Test class for CBAMZarrReader."""
+
+    fixtures = [
+        '2.provider.json',
+        '3.observation_type.json',
+        '4.dataset_type.json',
+        '5.dataset.json',
+        '6.unit.json',
+        '7.attribute.json',
+        '8.dataset_attribute.json'
+    ]
+
+    def setUp(self):
+        """Setup test cbam zarr reader."""
+        self.dataset = Dataset.objects.get(name='CBAM Climate Reanalysis')
+        self.attribute1 = DatasetAttribute.objects.filter(
+            dataset=self.dataset,
+            attribute__variable_name='max_temperature'
+        ).first()
+        self.dt1 = np.datetime64('2024-09-01')
+        self.dt2 = np.datetime64('2024-09-01')
+        self.xrDataset = xr.Dataset(
+            {
+                'max_total_temperature': (
+                    ('date', 'lat', 'lon'),
+                    [
+                        [
+                            [0.26790932, 0.5398054],
+                            [0.74384186, 0.50810691]
+                        ]
+                    ]
+                ),
+            },
+            coords={
+                'date': pd.date_range('2024-09-01', periods=1),
+                'lat': [0, 1],
+                'lon': [0, 1],
+            }
+        )
+        dt = datetime(2024, 9, 1, 0, 0, 0)
+        self.reader = CBAMZarrReader(
+            self.dataset, [self.attribute1],
+            DatasetReaderInput.from_point(Point(0, 1)),
+            dt, dt
+        )
+
+    def test_read_from_point(self):
+        """Test read cbam data from single point."""
+        ds = self.reader._read_variables_by_point(
+            self.xrDataset, ['max_total_temperature'],
+            self.dt1, self.dt2
+        )
+        self.assertAlmostEqual(ds['max_total_temperature'], 0.74384186, 2)
+
+    def test_read_from_bbox(self):
+        """Test read cbam data from bbox."""
+        self.reader.location_input = DatasetReaderInput.from_bbox(
+            [0, 0, 1, 1])
+        ds = self.reader._read_variables_by_bbox(
+            self.xrDataset, ['max_total_temperature'],
+            self.dt1, self.dt2
+        )
+        self.assertTrue(
+            np.allclose(
+                self.xrDataset['max_total_temperature'].values,
+                ds['max_total_temperature'].values
+            )
+        )
+
+    def test_read_from_polygon(self):
+        """Test read cbam data from polygon."""
+        self.reader.location_input = DatasetReaderInput(
+            MultiPolygon([Polygon.from_bbox((-1, -1, 2, 2))]),
+            LocationInputType.POLYGON
+        )
+        ds = self.reader._read_variables_by_polygon(
+            self.xrDataset, ['max_total_temperature'],
+            self.dt1, self.dt2
+        )
+        self.assertTrue(
+            np.allclose(
+                self.xrDataset['max_total_temperature'].values,
+                ds['max_total_temperature'].values
+            )
+        )
+
+    def test_read_from_points(self):
+        """Test read cbam data from list of point."""
+        self.reader.location_input = DatasetReaderInput(
+            MultiPoint([
+                Point(x=0, y=0),
+                Point(x=1, y=1)
+            ]),
+            LocationInputType.LIST_OF_POINT
+        )
+        ds = self.reader._read_variables_by_points(
+            self.xrDataset, ['max_total_temperature'],
+            self.dt1, self.dt2
+        )
+        print(ds)
+        val = ds['max_total_temperature'].values
+        print(val)
+        self.assertAlmostEqual(val[0][0][0], 0.26790932)
+        self.assertAlmostEqual(val[0][1][1], 0.50810691)
