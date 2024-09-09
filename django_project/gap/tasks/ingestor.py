2ed5e4ada4c7eb9856589082fb6ceb047a08acf5
Irwan Fathurrahman <meomancer@gmail.com>
2024-09-09 10:06:51 +0700

    Add arable-ingestor-session task every night

--- django_project/gap/tasks/ingestor.py
+++ django_project/gap/tasks/ingestor.py
@@ -8,7 +8,7 @@ Tomorrow Now GAP.
 from celery.utils.log import get_task_logger
 
 from core.celery import app
-from gap.models.ingestor import IngestorSession
+from gap.models.ingestor import IngestorSession, IngestorType
 
 logger = get_task_logger(__name__)
 
@@ -21,3 +21,19 @@ def run_ingestor_session(_id: int):
         session.run()
     except IngestorSession.DoesNotExist:
         logger.error('Ingestor Session {} does not exists'.format(_id))
+
+
+@app.task(name='arable_ingestor_session')
+def run_arable_ingestor_session():
+    """Run Ingestor for arable."""
+    session = IngestorSession.objects.filter(
+        ingestor_type=IngestorType.ARABLE
+    ).first()
+    if not session:
+        # When created, it is autorun
+        IngestorSession.objects.create(
+            ingestor_type=IngestorType.ARABLE
+        )
+    else:
+        # When not created, it is run manually
+        session.run()
