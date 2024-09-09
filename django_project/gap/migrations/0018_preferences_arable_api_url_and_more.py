71de8cbca6a7dc669a69266ce3dbedcc4a3dfd16
Irwan Fathurrahman <meomancer@gmail.com>
2024-09-09 10:20:37 +0700

    Fix migrations

--- django_project/gap/migrations/0018_preferences_arable_api_url_and_more.py
+++ django_project/gap/migrations/0018_preferences_arable_api_url_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('gap', '0016_datasourcefile_is_latest_datasourcefile_metadata'),
+        ('gap', '0017_preferences_salient_area'),
     ]
 
     operations = [
