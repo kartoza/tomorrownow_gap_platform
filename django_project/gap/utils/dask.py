71fe2f09a4ef895f4041e99f131ca6622443bf22
Danang Massandy <danangmassandy@gmail.com>
2024-12-04 22:58:09 +0000

    use chunks to avoid loading all dataframe in memory

--- django_project/gap/utils/dask.py
+++ django_project/gap/utils/dask.py
@@ -12,6 +12,20 @@ from concurrent.futures import ThreadPoolExecutor
 from gap.models import Preferences
 
 
+def get_num_of_threads(is_api=False):
+    """Get number of threads for dask computation.
+
+    :param is_api: whether for API usage, defaults to False
+    :type is_api: bool, optional
+    """
+    preferences = Preferences.load()
+    return (
+        preferences.dask_threads_num_api if is_api else
+        preferences.dask_threads_num
+    )
+
+
+
 def execute_dask_compute(x: Delayed, is_api=False):
     """Execute dask computation based on number of threads config.
 
@@ -20,11 +34,7 @@ def execute_dask_compute(x: Delayed, is_api=False):
     :param is_api: Whether the computation is in GAP API, default to False
     :type is_api: bool
     """
-    preferences = Preferences.load()
-    num_of_threads = (
-        preferences.dask_threads_num_api if is_api else
-        preferences.dask_threads_num
-    )
+    num_of_threads = get_num_of_threads(is_api)
     if num_of_threads <= 0:
         # use everything
         x.compute()
