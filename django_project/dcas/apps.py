dabdfb25709ae2581cb2456e330ba4ac17c92e5f
Danang Massandy <danangmassandy@gmail.com>
2024-12-18 01:03:53 +0000

    add dcas initial app

---
+++ django_project/dcas/apps.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+"""
+Tomorrow Now GAP.
+
+.. note:: DCAS AppConfig
+"""
+from django.apps import AppConfig
+
+
+class DcasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dcas'
---
+++ django_project/dcas/models/__init__.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+"""
+Tomorrow Now GAP.
+
+.. note:: Models
+"""
+
+from dcas.models.config import *  # noqa
