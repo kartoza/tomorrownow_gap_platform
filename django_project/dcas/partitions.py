f765269b59948f00b1ffec2dcc4080621db9fd1f
Danang Massandy <danangmassandy@gmail.com>
2025-01-17 07:36:54 +0000

    merge base and cap temperature from GDDConfig model

--- django_project/dcas/partitions.py
+++ django_project/dcas/partitions.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 
 from gap.models import Attribute
-from dcas.models import DCASConfig
+from dcas.models import DCASConfig, GDDConfig
 from dcas.rules.rule_engine import DCASRuleEngine
 from dcas.utils import read_grid_data
 from dcas.functions import (
@@ -46,13 +46,8 @@ def process_partition_total_gdd(
     # merge the df with grid_data
     df = df.merge(grid_data_df, on=['grid_id'], how='inner')
 
-    # TODO: change from configuration based on crop
-    df['gdd_base'] = np.random.random_integers(
-        low=5, high=15, size=df.shape[0]
-    )
-    df['gdd_cap'] = np.random.random_integers(
-        low=25, high=40, size=df.shape[0]
-    )
+    # merge with base and cap temperature in gdd config
+    df = _merge_partition_gdd_config(df)
 
     # add new column to normalize max and min temperature
     norm_temperature = {}
@@ -280,3 +275,32 @@ def process_partition_message_output(
     )
 
     return df
+
+
+def _merge_partition_gdd_config(df: pd.DataFrame) -> pd.DataFrame:
+    """Merge dataframe with GDD config: base and cap temperature.
+
+    :param df: input DataFrame that has column: config_id and crop_id
+    :type df: pd.DataFrame
+    :return: dataframe with new columns: gdd_base, gdd_cap
+    :rtype: pd.DataFrame
+    """
+    crop_list = []
+    config_list = []
+    base_list = []
+    cap_list = []
+    configs = GDDConfig.objects.all().order_by('config_id', 'crop_id')
+    for gdd_config in configs:
+        config_list.append(gdd_config.config.id)
+        crop_list.append(gdd_config.crop.id)
+        base_list.append(gdd_config.base_temperature)
+        cap_list.append(gdd_config.cap_temperature)
+
+    gdd_config_df = pd.DataFrame({
+        'crop_id': crop_list,
+        'config_id': config_list,
+        'gdd_base': base_list,
+        'gdd_cap': cap_list
+    })
+
+    return df.merge(gdd_config_df, how='inner', on=['crop_id', 'config_id'])
--- django_project/dcas/tests/test_pipeline.py
+++ django_project/dcas/tests/test_pipeline.py
@@ -6,67 +6,14 @@ Tomorrow Now GAP.
 """
 
 import pandas as pd
-import datetime
-from django.test import TestCase
 
 from dcas.models import DCASConfig, DCASConfigCountry
 from dcas.pipeline import DCASDataPipeline
-from gap.factories import (
-    FarmRegistryGroupFactory, FarmRegistryFactory,
-    FarmFactory, GridFactory, CountryFactory
-)
+from dcas.tests.base import DCASPipelineBaseTest
 
 
-class DCASPipelineTest(TestCase):
+class DCASPipelineTest(DCASPipelineBaseTest):
     """DCAS Pipeline test case."""
-
-    fixtures = [
-        '2.provider.json',
-        '3.station_type.json',
-        '4.dataset_type.json',
-        '5.dataset.json',
-        '6.unit.json',
-        '7.attribute.json',
-        '8.dataset_attribute.json',
-        '1.dcas_config.json',
-        '12.crop_stage_type.json',
-        '13.crop_growth_stage.json',
-        '14.crop.json',
-        '15.gdd_config.json',
-        '16.gdd_matrix.json'
-    ]
-
-    def setUp(self):
-        """Set DCASPipelineTest class."""
-        country_1 = CountryFactory(
-            iso_a3='KE'
-        )
-        country_2 = CountryFactory(
-            iso_a3='ZAF'
-        )
-        self.grid_1 = GridFactory(
-            country=country_1
-        )
-        self.grid_2 = GridFactory(
-            country=country_2
-        )
-
-        self.default_config = DCASConfig.objects.get(id=1)
-        self.request_date = datetime.date(2025, 1, 15)
-
-        # create farm registries
-        self.farm_registry_group = FarmRegistryGroupFactory()
-        self._create_farm_registry_for_grid(
-            self.farm_registry_group, self.grid_1)
-        self._create_farm_registry_for_grid(
-            self.farm_registry_group, self.grid_2)
-
-    def _create_farm_registry_for_grid(self, group, grid):
-        farm = FarmFactory(grid=grid)
-        return FarmRegistryFactory(
-            group=group,
-            farm=farm
-        )
 
     def test_merge_grid_data_with_config(self):
         """Test merge_grid_data_with_config."""
