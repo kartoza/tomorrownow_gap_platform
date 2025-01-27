4e16e885fe15a5ec2947689b068c2b64a460d6b4
Danang Massandy <danangmassandy@gmail.com>
2025-01-27 20:16:38 +0000

    fix test

--- django_project/dcas/tests/test_utils.py
+++ django_project/dcas/tests/test_utils.py
@@ -8,11 +8,37 @@ Tomorrow Now GAP.
 from mock import patch, MagicMock
 
 from dcas.tests.base import DCASPipelineBaseTest
-from dcas.utils import read_grid_crop_data
+from dcas.utils import read_grid_crop_data, read_grid_data
 
 
 class DCASUtilsTest(DCASPipelineBaseTest):
     """DCAS utilities test case."""
+
+    @patch('dcas.outputs.duckdb.connect')
+    def test_read_grid_data(self, mock_duckdb_connect):
+        """Test read_grid_data function."""
+        # Mock the connection object
+        mock_conn = MagicMock()
+        mock_duckdb_connect.return_value = mock_conn
+
+        read_grid_data('test.parquet', ['column_1'], [1])
+
+        mock_duckdb_connect.assert_called_once()
+        mock_conn.sql.assert_called_once()
+        mock_conn.close.assert_called_once()
+
+    @patch('dcas.outputs.duckdb.connect')
+    def test_read_grid_data_with_threads(self, mock_duckdb_connect):
+        """Test read_grid_data function with threads."""
+        # Mock the connection object
+        mock_conn = MagicMock()
+        mock_duckdb_connect.return_value = mock_conn
+
+        read_grid_data('test.parquet', ['column_1'], [1], 2)
+
+        mock_duckdb_connect.assert_called_once()
+        mock_conn.sql.assert_called_once()
+        mock_conn.close.assert_called_once()
 
     @patch('dcas.outputs.duckdb.connect')
     def test_read_grid_crop_data(self, mock_duckdb_connect):
