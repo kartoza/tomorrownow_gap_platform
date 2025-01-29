ce0eb78021a4d733acab9d9a591556d5d8cf2324
Danang Massandy <danangmassandy@gmail.com>
2025-01-29 05:07:53 +0000

    update dcas request model

--- django_project/dcas/models/request.py
+++ django_project/dcas/models/request.py
@@ -9,6 +9,7 @@ from django.contrib.gis.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
+from core.models.background_task import TaskStatus
 from gap.models.common import Country
 
 
@@ -20,7 +21,10 @@ class DCASRequest(models.Model):
         help_text="The time when the request was created."
     )
     country = models.ForeignKey(
-        Country, on_delete=models.CASCADE
+        Country,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
     )
     start_time = models.DateTimeField(
         null=True,
@@ -30,6 +34,20 @@ class DCASRequest(models.Model):
         null=True,
         blank=True
     )
+    status = models.CharField(
+        max_length=255,
+        choices=TaskStatus.choices,
+        default=TaskStatus.PENDING,
+        null=True,
+        blank=True,
+        help_text="The status of the process."
+    )
+    progress_text = models.TextField(
+        null=True,
+        blank=True,
+        help_text="Progress or note from the pipeline."
+    )
+    config = models.JSONField(blank=True, default=dict, null=True)
 
     class Meta:  # noqa
         """Meta class for DCASRequest."""
